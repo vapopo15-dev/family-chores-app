2ee0101b6e863ca229f9965779d3df0cd981990a
vapopo15-dev <vapopo15@gmail.com>
2026-03-14 00:16:44 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,38 @@
+const CACHE_NAME = "app-cache-v2"; // ← バージョンを上げる
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json"
+];
+
+// インストール時にキャッシュ
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open("app-cache").then((cache) => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./style.css",
-        "./script.js",
-        "./manifest.json"
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
     })
   );
+  self.skipWaiting(); // 新バージョンを即座に有効化
 });
 
+// 古いキャッシュを削除
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim(); // すぐに新SWを適用
+});
+
+// キャッシュ優先で返す
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
